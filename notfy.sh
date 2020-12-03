6bb3e9182d47bf80f94f3f2a01c7da2b94bc5472
Kabilan S <45006244+kabilan1290@users.noreply.github.com>
2020-12-04 00:44:47 +0530

    Update notfy.sh

--- notfy.sh
+++ notfy.sh
@@ -1,1 +1,1 @@
-
+curl --request POST --url $U --user $AUTH --header $H1 --header $H2 --data '{"fields":{"summary":"CodeBuild - Build Failed","issuetype":{"name":"Bug"},"project":{"key":"KAB"},"description":{"type":"doc","version":1,"content":[{"type":"paragraph","content":[{"text":"Build ID:'$CODEBUILD_BUILD_ID' \n Build Number:'$CODEBUILD_BUILD_NUMBER'","type":"text"}]}]}}}'
